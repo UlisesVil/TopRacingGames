bf429779c22cf5f3aed2940e85bdbab81f11c2d7
github_UlisesVil <ulisesvil5@hotmail.com>
2021-02-22 04:43:47 -0600

    Auto-generated commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,47 +4,45 @@ $(document).ready(function() {
 
     //Nav-Bar
     $(".mat-drawer-backdrop").click(function(){
-        console.log("Me llega el click del wall");
-        $('#transparentWall').removeAttr('style','background:red; width: 100%; height: 100vh; ');
-        $('mat-toolbar').removeAttr('style','background:red;');
+        $('#transparentWall').removeAttr('style','background:black; width: 100%; height: 100vh; ');
+        $('mat-toolbar').removeAttr('style','background:black;');
+        $('#pngmenu').removeAttr('src','../assets/img/car3.png');
+        $('#pngmenu').removeAttr('style','width: 100%; margin-top: 150px;');
     });
     $(".linkNav").click(function(){
-        console.log("Me llega el click del link");
-        $('#transparentWall').removeAttr('style','background:red; width: 100%; height: 100vh; ');
-        $('mat-toolbar').removeAttr('style','background:red; display:none');
-           
+        $('#transparentWall').removeAttr('style','background:black; width: 100%; height: 100vh; ');
+        $('mat-toolbar').removeAttr('style','background:black; display:none');
+        $('#pngmenu').removeAttr('src','../assets/img/car3.png');
+        $('#pngmenu').removeAttr('style','width: 100%; margin-top: 150px;');  
     });
     $("#buttonNav2").click(function(){
-        console.log('recibo el clic del 2do botton');
-        $('#transparentWall').removeAttr('style','background:red; width: 100%; height: 100vh; ');
-        $('mat-toolbar').removeAttr('style','background:red; display:none');
+        $('#transparentWall').removeAttr('style','background:black; width: 100%; height: 100vh; ');
+        $('mat-toolbar').removeAttr('style','background:black; display:none');
+        $('#pngmenu').removeAttr('src','../assets/img/car3.png');
+        $('#pngmenu').removeAttr('style','width: 100%; margin-top: 150px;');
     });
-   
+    
     //Personalized Button Upload  
     $('input[type=file]').change(function(){
         var filename = $(this).val().split('\\').pop();
         var idname = $(this).attr('id');
-        console.log($(this));
-        console.log(filename);
-        console.log(idname);
         $('span.'+idname).next().find('span').html(filename);
     }); 
+
 });  
 
-
+//Resize #titleContent main.component.html
 $(window).ready(function(){
-    var width = $(window).width();   
-    console.log(width);
+    var width = $(window).width();
     $('#title').height(width*.575);
 });
 
+//Video progress-control with scroll main.component
 $(window).scroll(function(){
     let scrollTop = document.documentElement.scrollTop;
     let videoIntro = document.querySelector("#videoIntro"); 
-    
     if(videoIntro){
         videoIntro.currentTime = scrollTop/500;
-        
     }
 });
 
